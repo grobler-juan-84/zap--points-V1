71bef8c62d6c9c3b53d068965f5b67a62b92f0d7
kisdreamteam <kisdreamteam@gmail.com>
2026-07-20 14:55:11 +0700

    LandingNavLink

--- src/features/landing/components/LandingHeader.tsx
+++ src/features/landing/components/LandingHeader.tsx
@@ -4,7 +4,7 @@ import LandingNavLink from '@/features/landing/components/LandingNavLink'
 export default function LandingHeader() {
   return (
     <header className="flex flex-row w-full justify-end items-end pr-[clamp(1.5rem,4vw,3.25rem)] gap-[clamp(1.5rem,3vw,3.75rem)]">
-      <div className="flex flex-row text-[clamp(1rem,3.0vw,2.75rem)] gap-[clamp(1rem,2vw,3rem)] text-lg text-white font-league-spartan font-bold">
+      <div className="flex flex-row gap-[clamp(1rem,2vw,3rem)]">
         <LandingNavLink href="/login">Login</LandingNavLink>
         <LandingNavLink href="/signup">Signup</LandingNavLink>
       </div>
